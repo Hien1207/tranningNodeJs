4c6fc9090bc27ecb6f78e7efb25803e78077e647
hientt <tranthihien12701@gmail.com>
2023-04-13 21:00:08 +0700

    prepared deploy

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 const cors = require('cors');
 
 const AppError = require('./utils/appError');
@@ -85,6 +86,8 @@ app.use(
     ],
   })
 );
+
+app.use(compression());
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
